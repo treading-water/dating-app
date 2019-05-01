eddaee8453dbcef82f18bef1accd24eb367b1d6b
Zach Butler <zach.standley93@gmail.com>
2019-05-01 15:43:15 -0700

    reworked API utility. Passing tests

--- test/applicant-api.test.js
+++ test/applicant-api.test.js
@@ -1,11 +1,18 @@
 import applicantApi from '../src/applicant-api.js';
 const test = QUnit.test;
 
+QUnit.module('applicant api');
+
+applicantApi.storage = sessionStorage;
+const testStorage = sessionStorage;
+
+
 test('round-trip applicant', (assert) => {
+    testStorage.removeItem('applicants');
     //Arrange
     // Set up your parameters and expectations
-    const applicant = { name: '' };
-
+    const applicant = { name: 'tester' };
+    
     //Act 
     // Call the function you're testing and set the result to a const
     applicantApi.save(applicant);
@@ -13,4 +20,28 @@ test('round-trip applicant', (assert) => {
     
     //Assert
     assert.deepEqual(result, applicant);
+});
+
+test('no applicant so return empty array', assert =>{
+    testStorage.removeItem('applicants');
+    const expected = [];
+
+    const applicants = applicantApi.getAll();
+
+    assert.deepEqual(applicants, expected);
+
+});
+
+test('two saves so return array with both objects', assert => {
+    testStorage.removeItem('applicants');
+    const applicant1 = { name: 'tester1' };
+    const applicant2 = { name: 'tester2' };
+    const expected = [applicant1, applicant2];
+
+    applicantApi.save(applicant1);
+    applicantApi.save(applicant2);
+
+    const applicants = applicantApi.getAll();
+
+    assert.deepEqual(applicants, expected);
 });
