b5dbd73fe3e8a959f427d187d151736910ffeac2
Claire Cassidy <clacassidy@gmail.com>
2019-05-03 13:52:38 -0700

    fixed yes/no on the application page

--- src/get-applicant.js
+++ src/get-applicant.js
@@ -1,6 +1,6 @@
 function getApplicant(formData) {
     //convert data to variables
-    const likesBurrito = formData.get('burrito') === 'yes';
+    const likesBurrito = formData.get('burrito');
     const cat = parseInt(formData.get('cat'));
     const catNumber = parseInt(formData.get('cat-number'));
 
